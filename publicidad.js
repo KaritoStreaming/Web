d37e2ab23cb40a7be14b5a6c3e3310bdec991d7e
KaritoStreaming <karitostreamingpagina@gmail.com>
2024-10-26 22:08:57 -0500

    Add files via upload

---
+++ publicidad.js
@@ -0,0 +1,9 @@
+// Muestra el popup de inmediato
+window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+};
+
+// Función para cerrar el popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
